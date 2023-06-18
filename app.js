a6d23635618f3d76051297bd2d8bae7e1dfbf94a
casval-deikun <64556091+casval-deikun@users.noreply.github.com>
2023-06-18 16:11:51 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
 // 카이카스 지갑 연결
 async function connectWallet() {
-    if (window.klaytn) {
-      try {
-        await window.klaytn.enable();
-        alert('Wallet connected!');
-      } catch (error) {
-        console.error('Failed to connect wallet:', error);
-      }
-    } else {
-      alert('Please install Klaytn wallet extension.');
+  if (window.klaytn) {
+    try {
+      await window.klaytn.enable();
+      alert('Wallet connected!');
+    } catch (error) {
+      console.error('Failed to connect wallet:', error);
     }
+  } else {
+    alert('Please install Klaytn wallet extension.');
   }
-  
-  // Klay 전송
-  async function sendKlay() {
-    if (window.klaytn) {
-      try {
-        const fromAddress = await window.klaytn.selectedAddress;
-        const toAddress = '0x1a179E7A37E6A39dfFA5a77e7B6467E693945881';
-        const value = '100000000000000000000'; // 100 Klay (wei 단위)
-  
-        const txHash = await window.klaytn.sendTransaction({
-          type: 'VALUE_TRANSFER',
-          from: fromAddress,
-          to: toAddress,
-          value: value,
-        });
-  
-        alert('Transaction sent! TxHash: ' + txHash);
-      } catch (error) {
-        console.error('Failed to send transaction:', error);
-      }
-    } else {
-      alert('Please connect Klaytn wallet.');
+}
+
+// Klay 전송
+async function sendKlay() {
+  if (window.klaytn) {
+    try {
+      const fromAddress = await window.klaytn.selectedAddress;
+      const toAddress = '0x1a179E7A37E6A39dfFA5a77e7B6467E693945881';
+      const value = '100000000000000000000'; // 100 Klay (wei 단위)
+
+      const transactionObject = {
+        type: 'VALUE_TRANSFER',
+        from: fromAddress,
+        to: toAddress,
+        value: value,
+      };
+
+      const txHash = await window.klaytn.sendTransaction(transactionObject);
+      alert('Transaction sent! TxHash: ' + txHash);
+    } catch (error) {
+      console.error('Failed to send transaction:', error);
     }
+  } else {
+    alert('Please connect Klaytn wallet.');
   }
-  
-  // 버튼 클릭 이벤트 핸들러 등록
-  $(document).ready(function() {
-    $('#connectButton').click(connectWallet);
-    $('#sendButton').click(sendKlay);
-  });
-  
+}
+
+// 버튼 클릭 이벤트 핸들러 등록
+$(document).ready(function() {
+  $('#connectButton').click(connectWallet);
+  $('#sendButton').click(sendKlay);
+});
